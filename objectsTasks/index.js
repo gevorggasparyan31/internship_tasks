155b715ab0f8a76c61f4d44212b219b527adc25b
gev.org <thecarguyarm@gmail.com>
2023-08-24 11:15:47 +0400

    edited the curly brackets and jsdoc

--- objectsTasks/index.js
+++ objectsTasks/index.js
@@ -8,21 +8,24 @@ let user = {
 checkProperty(user,"city");
 console.log(JSON.stringify(swapKeyAndValue(user)));
 
-//TASK 1 Check if object contains a given property
 /**
+ * TASK 1 Check if object contains a given property
  *
  * @param obj - is an object whose property function checks
  * @param prop - property which must be checked
  */
 function checkProperty(obj, prop) {
-    if (prop in obj)
-            console.log("Object has the property "+prop);
-         else
-            console.log("Object doesn't have the given property");
+    if (prop in obj) {
+        console.log("Object has the property " + prop);
+    }
+         else {
+        console.log("Object doesn't have the given property");
+    }
 }
 
-// TASK 2 Get a copy of the object where the keys become the values and the values are the keys
 /**
+ * TASK 2 Get a copy of the object where the keys become the values and the values are the keys
+ *
  *
  * @param obj - original object
  * @returns {{}} -  copied object with already swapped keys and values
