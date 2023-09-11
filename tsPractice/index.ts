3e8e1cafbe71ca5be51e0ac03a4ec6052026aba5
gev.org <thecarguyarm@gmail.com>
2023-09-11 16:48:35 +0400

    updates

--- tsPractice/index.ts
+++ tsPractice/index.ts
@@ -2,6 +2,8 @@ const a:number[] = [4,2,5,60,10,9,15];
 const b:number[] = [67,12,0,2,1,0];
 const v = 22;
 
+console.log(primitiveSumPairExists(a,b,v));
+console.log(objectSumPairExists(a,b,v));
 function primitiveSumPairExists(a,b,v):boolean{
     for(let i = 0; i < a.length; i++) {
         const target = v - a[i];
@@ -14,9 +16,9 @@ function primitiveSumPairExists(a,b,v):boolean{
     return false;
 }
 function objectSumPairExists (a,b,v):boolean {
-    const objB = b.reduce(function (aggr,value){
-        aggr[value] = true;
-        return aggr;
+    const objB = b.reduce(function (aggregation,value){
+        aggregation[value] = true;
+        return aggregation;
     },{});
 
     for (let i = 0; i < a.length; i++) {
